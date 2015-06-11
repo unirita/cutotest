e884624a98eae7b98408a00ed1d6fe314dfb0347
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-06-11 20:28:30 +0900

    Add call logic to regression test code.

--- regression/regression_test.go
+++ regression/regression_test.go
@@ -9,7 +9,17 @@ import (
 func TestRegression(t *testing.T) {
 	util.InitCutoRoot()
 	util.DeployTestData("regression")
-	t.Log(util.ComplementConfig("master.ini"))
-	//m := util.NewMaster()
-	//s := util.NewServant()
+	util.ComplementConfig("master.ini")
+	util.ComplementConfig("servant.ini")
+
+	s := util.NewServant()
+	s.SetConfig("servant.ini")
+	if err := s.Start(); err != nil {
+		t.Fatalf("Servant start failed: %s", err)
+	}
+	defer s.Kill()
+
+	m := util.NewMaster()
+	m.SetConfig("master.ini")
+	m.Run("inst_test")
 }
