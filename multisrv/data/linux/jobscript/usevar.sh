a457c5e57dc9a5e08cee5b9410621c382b6bb49b
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-06-18 15:06:09 +0900

    Add multi server execution test data for linux.

---
+++ multisrv/data/linux/jobscript/usevar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "FLOWID=$1"
+echo "FLOWSD=$FLOWSD"
+echo "SSROOT=$SSROOT"
+echo "MEPATH=$MEPATH"
+echo "SEPATH=$SEPATH"
+echo "JOUT=$JOUT"
+echo "JRC=$JRC"
+echo "JSD=$JSD"
+echo "JED=$JED"
