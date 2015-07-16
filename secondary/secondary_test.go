a00b88564fd8b0ead2543401237b05eddb6e15b4
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-07-16 10:33:22 +0900

    Add test case.

--- secondary/secondary_test.go
+++ secondary/secondary_test.go
@@ -38,6 +38,32 @@ func TestLegacy(t *testing.T) {
 }
 
 func TestNoRetry(t *testing.T) {
+	defer util.SaveEvidence("secondary", "noretry")
+	util.InitCutoRoot()
+	util.DeployTestData("secondary")
+
+	s := util.NewServant()
+	s.UseConfig("servant.ini")
+	if err := s.Start(); err != nil {
+		t.Fatalf("Servant start failed: %s", err)
+	}
+	defer s.Kill()
+
+	m := util.NewMaster()
+	m.UseConfig("master_noretry.ini")
+	_, err := m.Run("nosecondary")
+	if err != nil {
+		t.Fatalf("Master run failed: %s", err)
+	}
+	if !util.ContainsInFile(masterLog, "CTM025W") {
+		t.Errorf("Job did not end abnormally.")
+	}
+	if util.ContainsInFile(masterLog, "CTM027W") {
+		t.Errorf("Retried job execution unexpectedly.")
+	}
+	if util.ContainsInFile(masterLog, "CTM028W") {
+		t.Errorf("Executed job at secondary servant unexpectedly.")
+	}
 
 }
 
