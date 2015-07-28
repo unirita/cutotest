591ded12ed2082903e18e256cd6dafd69e61d20d
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-07-28 09:28:38 +0900

    Add realtime testcase.
    Parallel flow test with JSON only.

--- realtime/handler.go
+++ realtime/handler.go
@@ -15,3 +15,14 @@ func outputSerialFlow() http.Handler {
 	}
 	return http.HandlerFunc(f)
 }
+
+func outputParallelFlow() http.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		job1 := makeBatchFileName("job1")
+		job2 := makeBatchFileName("job2")
+		job3 := makeBatchFileName("job3")
+		fmt.Fprintf(w, `{"flow":"[%s,%s->%s]"}`, job1, job2, job3)
+
+	}
+	return http.HandlerFunc(f)
+}
--- realtime/realtime_test.go
+++ realtime/realtime_test.go
@@ -44,3 +44,40 @@ func TestJSONOnly_Serial(t *testing.T) {
 	waitProcessByPID(res.PID)
 	assertIsJob1to3Successed(t)
 }
+
+func TestJSONOnly_Parallel(t *testing.T) {
+	defer util.SaveEvidence("realtime", "json_only", "parallel")
+	util.InitCutoRoot()
+	util.DeployTestData("realtime")
+
+	jobnetDir := filepath.Join(util.GetCutoRoot(), "bpmn")
+	util.ClearDir(jobnetDir)
+
+	s := util.NewServant()
+	s.UseConfig("servant.ini")
+	if err := s.Start(); err != nil {
+		t.Fatalf("Servant start failed: %s", err)
+	}
+	defer s.Kill()
+
+	ts := httptest.NewServer(outputParallelFlow())
+	defer ts.Close()
+
+	r := util.NewRealtime()
+	rc, err := r.Run(ts.URL)
+	if err != nil {
+		t.Fatalf("Unexpected error occured: %s", err)
+	}
+	if rc != 0 {
+		t.Log(r.Stdout)
+		t.Fatalf("rc => %d, want %d", rc, 0)
+	}
+
+	res := parseRealtimeResult(t, r.Stdout)
+	assertSuccessRealtimeOutput(t, res)
+	assertNotRemainNetworkFile(t)
+	assertIsNotNetworkEnds(t)
+
+	waitProcessByPID(res.PID)
+	assertIsJob1to3Successed(t)
+}
