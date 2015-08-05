77e38bfec5ba0dbb06c10be8d6802d5538f98d40
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-08-05 17:21:52 +0900

    Add test case.

--- timezone/timezone_linux_test.go
+++ timezone/timezone_linux_test.go
@@ -51,8 +51,18 @@ func isTimeUTC(timeStr string) bool {
 	if err != nil {
 		return false
 	}
-	diff := now.Sub(target)
-	return diff.Seconds() < 300 && -300 < diff.Seconds()
+	diffSec := now.Sub(target).Seconds()
+	return diffSec < 300 && -300 < diffSec
+}
+
+func isTimeLocal(timeStr string) bool {
+	now := time.Now()
+	target, err := time.Parse(timeFormat, timeStr)
+	if err != nil {
+		return false
+	}
+	diffSec := now.Sub(target).Seconds()
+	return diffSec < 300 && -300 < diffSec
 }
 
 func TestNetworkTimestamp_UTC(t *testing.T) {
@@ -78,6 +88,32 @@ func TestNetworkTimestamp_UTC(t *testing.T) {
 	}
 	if !isTimeUTC(testnet.UpdateDate) {
 		t.Error("Network record updatedate is not utc.")
+	}
+}
+
+func TestNetworkTimestamp_Local(t *testing.T) {
+	output := runShow(t, false)
+	jsData, err := parseShowOutput(output)
+	if err != nil {
+		t.Log("Failed to parse output of show utility.")
+		t.Fatalf("Reason: %s", err)
+	}
+
+	testnet := findNetworkResult(jsData.Jobnetworks, "timezone")
+	if testnet == nil {
+		t.Fatalf("Network result not found.", "timezone")
+	}
+	if !isTimeLocal(testnet.StartDate) {
+		t.Error("Network startdate is not local timezone.")
+	}
+	if !isTimeLocal(testnet.EndDate) {
+		t.Error("Network enddate is not local timezone.")
+	}
+	if !isTimeLocal(testnet.CreateDate) {
+		t.Error("Network record createdate is not local timezone.")
+	}
+	if !isTimeLocal(testnet.UpdateDate) {
+		t.Error("Network record updatedate is not local timezone.")
 	}
 }
 
@@ -112,6 +148,37 @@ func TestJobTimestamp_UTC(t *testing.T) {
 	}
 }
 
+func TestJobTimestamp_Local(t *testing.T) {
+	output := runShow(t, false)
+	jsData, err := parseShowOutput(output)
+	if err != nil {
+		t.Log("Failed to parse output of show utility.")
+		t.Fatalf("Reason: %s", err)
+	}
+
+	testnet := findNetworkResult(jsData.Jobnetworks, "timezone")
+	if testnet == nil {
+		t.Errorf("Network result not found.", "timezone")
+	}
+	if len(testnet.Jobs) != 3 {
+		t.Fatalf("len(testnet.Jobs) => %d, wants %d.", len(testnet.Jobs), 3)
+	}
+	for _, job := range testnet.Jobs {
+		if !isTimeLocal(job.StartDate) {
+			t.Errorf("Job[%s] startdate is not local timezone.", job.Jobname)
+		}
+		if !isTimeLocal(job.EndDate) {
+			t.Errorf("Job[%s] enddate is not local timezone.", job.Jobname)
+		}
+		if !isTimeLocal(job.CreateDate) {
+			t.Errorf("Job[%s] record createdate is not local timezone.", job.Jobname)
+		}
+		if !isTimeLocal(job.UpdateDate) {
+			t.Errorf("Job[%s] record updatedate is not local timezone.", job.Jobname)
+		}
+	}
+}
+
 func TestDateBorder_UTC(t *testing.T) {
 	output := runShow(t, true)
 	jsData, err := parseShowOutput(output)
@@ -128,6 +195,26 @@ func TestDateBorder_UTC(t *testing.T) {
 		yesterdaynet := findNetworkResult(jsData.Jobnetworks, "yesterday")
 		if yesterdaynet != nil {
 			t.Errorf("Network[%s] result must not found, but did it.", "yesterday")
+		}
+	}
+}
+
+func TestDateBorder_Local(t *testing.T) {
+	output := runShow(t, false)
+	jsData, err := parseShowOutput(output)
+	if err != nil {
+		t.Log("Failed to parse output of show utility.")
+		t.Fatalf("Reason: %s", err)
+	}
+	if isTodayInUTC() {
+		todaynet := findNetworkResult(jsData.Jobnetworks, "today")
+		if todaynet != nil {
+			t.Errorf("Network[%s] result must not found, but did it.", "today")
+		}
+	} else {
+		yesterdaynet := findNetworkResult(jsData.Jobnetworks, "yesterday")
+		if yesterdaynet == nil {
+			t.Errorf("Network[%s] result not found.", "yesterday")
 		}
 	}
 }
