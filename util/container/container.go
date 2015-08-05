3db8e6e054e96c582b53e91643f79e577455bb4a
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-08-05 09:43:33 +0900

    Enable util/container use freely-selected timezone.

--- util/container/container.go
+++ util/container/container.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -9,24 +10,41 @@ type Container struct {
 	image     string
 	name      string
 	isRunning bool
+	timezone  string
 }
+
+const (
+	defaultTimezone = "/etc/localtime"
+	zoneInfoPath    = "/usr/share/zoneinfo"
+)
 
 func New(image string, name string) *Container {
 	c := new(Container)
 	c.image = image
 	c.name = name
 	c.isRunning = false
+	c.timezone = defaultTimezone
 	return c
 }
 
 func (c *Container) Start() error {
-	err := exec.Command("docker", "run", "-itd", "--name", c.name, c.image).Run()
+	tzOption := fmt.Sprintf("%s:/etc/localtime:ro", c.timezone)
+	cmd := exec.Command("docker", "run", "-itd", "-v", tzOption, "--name", c.name, c.image)
+	err := cmd.Run()
 	if err != nil {
 		return err
 	}
 
 	c.isRunning = true
 	return nil
+}
+
+func (c *Container) SetTimezone(zoneName string) {
+	c.timezone = fmt.Sprintf("%s/%s", zoneInfoPath, zoneName)
+}
+
+func (c *Container) ResetTimezone() {
+	c.timezone = defaultTimezone
 }
 
 func (c *Container) IPAddress() (string, error) {
