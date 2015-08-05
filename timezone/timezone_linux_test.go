02df6c34dfc1f80ec64cf0dddb0d81464fb31035
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-08-05 17:09:11 +0900

    Add test case.

--- timezone/timezone_linux_test.go
+++ timezone/timezone_linux_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 const dateFormat = "20060102"
+const timeFormat = "2006-01-02 15:04:05.000"
 
 func runShow(t *testing.T, isUTC bool) string {
 	now := time.Now()
@@ -31,27 +32,30 @@ func runShow(t *testing.T, isUTC bool) string {
 	return show.Stdout
 }
 
-func extractNetworkResults(jobnets []*OutputJobNet) (*OutputJobNet, *OutputJobNet, *OutputJobNet) {
-	var testnet, todaynet, yesterdaynet *OutputJobNet
+func findNetworkResult(jobnets []*OutputJobNet, name string) *OutputJobNet {
 	for _, jobnet := range jobnets {
-		switch jobnet.Jobnetwork {
-		case "timezone":
-			testnet = jobnet
-		case "today":
-			todaynet = jobnet
-		case "yesterday":
-			yesterdaynet = jobnet
+		if jobnet.Jobnetwork == name {
+			return jobnet
 		}
 	}
-
-	return testnet, todaynet, yesterdaynet
+	return nil
 }
 
 func isTodayInUTC() bool {
 	return time.Now().Hour() >= 9
 }
 
-func TestNetworkTimestamp(t *testing.T) {
+func isTimeUTC(string timeStr) bool {
+	now := time.Now().UTC()
+	target, err := time.ParseInLocation(timeFormat, timeStr, time.UTC)
+	if err != nil {
+		return false
+	}
+	diff := now.Sub(target)
+	return diff.Seconds() < 300 && -300 < diff.Seconds()
+}
+
+func TestNetworkTimestamp_UTC(t *testing.T) {
 	output := runShow(t, true)
 	jsData, err := parseShowOutput(output)
 	if err != nil {
@@ -59,15 +63,55 @@ func TestNetworkTimestamp(t *testing.T) {
 		t.Fatalf("Reason: %s", err)
 	}
 
-	testnet, todaynet, yesterdaynet := extractNetworkResults(jsData.Jobnetworks)
+	testnet := findNetworkResult(jsData.Jobnetworks, "timezone")
 	if testnet == nil {
-		t.Errorf("Network[%s] result not found.", "timezone")
+		t.Errorf("Network result not found.", "timezone")
+	}
+	if !isTimeUTC(testnet.StartDate) {
+		t.Error("Network startdate is not utc.")
+	}
+	if !isTimeUTC(testnet.EndDate) {
+		t.Error("Network enddate is not utc.")
+	}
+	if !isTimeUTC(testnet.CreateDate) {
+		t.Error("Network record createdate is not utc.")
+	}
+	if !isTimeUTC(testnet.UpdateDate) {
+		t.Error("Network record updatedate is not utc.")
+	}
+}
+
+func TestJobTimestamp_UTC(t *testing.T) {
+	output := runShow(t, true)
+	jsData, err := parseShowOutput(output)
+	if err != nil {
+		t.Log("Failed to parse output of show utility.")
+		t.Fatalf("Reason: %s", err)
+	}
+
+	testnet := findNetworkResult(jsData.Jobnetworks, "timezone")
+	if testnet == nil {
+		t.Errorf("Network result not found.", "timezone")
+	}
+	if len(testnet.Jobs) != 3 {
+		t.Fatalf("len(testnet.Jobs) => %d, wants %d.", len(testnet.Jobs), 3)
+	}
+}
+
+func TestDateBorder_UTC(t *testing.T) {
+	output := runShow(t, true)
+	jsData, err := parseShowOutput(output)
+	if err != nil {
+		t.Log("Failed to parse output of show utility.")
+		t.Fatalf("Reason: %s", err)
 	}
 	if isTodayInUTC() {
+		todaynet := findNetworkResult(jsData.Jobnetworks, "today")
 		if todaynet == nil {
 			t.Errorf("Network[%s] result not found.", "today")
 		}
 	} else {
+		yesterdaynet := findNetworkResult(jsData.Jobnetworks, "yesterday")
 		if yesterdaynet != nil {
 			t.Errorf("Network[%s] result must not found, but did it.", "yesterday")
 		}
