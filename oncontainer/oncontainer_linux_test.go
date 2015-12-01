a00ee508740a72987810e5d97aa04c535cdb257b
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-12-01 14:49:18 +0900

    Fix test data for rerun test.

--- oncontainer/oncontainer_linux_test.go
+++ oncontainer/oncontainer_linux_test.go
@@ -47,13 +47,49 @@ func TestOnContainerJob_GetJoblog(t *testing.T) {
 		t.Fatalf("Master RC => %d, wants %d", rc, 0)
 	}
 
-	joblogs := util.FindJoblog("joblog", 1, "varout")
+	joblogs := util.FindJoblog("joblog", 2, "varout")
 	if len(joblogs) != 1 {
 		t.Fatalf("Number of joblog => %d, wants %d", len(joblogs), 1)
 	}
 
 	if !util.ContainsInFile(joblogs[0], "testparam") {
 		t.Error("Joblog was not output correctly.")
+	}
+}
+
+func TestOnContainerJob_RC(t *testing.T) {
+	defer util.SaveEvidence("oncontainer", "remote")
+	util.InitCutoRoot()
+	util.DeployTestData("oncontainer")
+
+	servant := util.NewServant()
+	servant.UseConfig("servant.ini")
+	if err := servant.Start(); err != nil {
+		t.Fatalf("Servant start failed: %s\n", err)
+	}
+	defer servant.Kill()
+
+	master := util.NewMaster()
+	master.UseConfig("master.ini")
+	rc, err := master.Run("rc")
+	if err != nil {
+		t.Fatalf("Master run failed: %s", err)
+	}
+	if rc != 0 {
+		t.Fatalf("Master RC => %d, wants %d", rc, 0)
+	}
+
+	conn, err := db.Open(util.GetDBDirPath())
+	if err != nil {
+		t.Fatalf("Database open failed: %s", err)
+	}
+	jobRecord, err := conn.SelectJob(2, "job1")
+	if err != nil {
+		t.Fatalf("Could not select job record: %s", err)
+	}
+
+	if jobRecord.RC != 123 {
+		t.Errorf("RC of job => %d, wants %d", jobRecord.RC, 123)
 	}
 }
 
@@ -83,12 +119,12 @@ func TestOnContainerJob_Remote(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Database open failed: %s", err)
 	}
-	jobRecord, err := conn.SelectJob(1, "job1")
+	jobRecord, err := conn.SelectJob(2, "job1")
 	if err != nil {
 		t.Fatalf("Could not select job record: %s", err)
 	}
 
 	if jobRecord.RC != 234 {
-		t.Errorf("RC of job => %d, wants %d", jobRecord.RC, 123)
+		t.Errorf("RC of job => %d, wants %d", jobRecord.RC, 234)
 	}
 }
